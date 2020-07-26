83ac93315143b20800b673a647dc6260b3ab4966
BohdanHorodyskyi <bohdanhorodyskyi@gmail.com>
2020-07-26 21:38:39 +0300

    second commit

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// $('.number1').animate({ num: 1386 - 3 }, {
-//     duration: 3000,
-//     step: function (number1){
-//         this.innerHTML = (number1 + 3).toFixed(0) 
-//     }
-// });
-// $('.number2').animate({ num: 386 - 3 }, {
-//     duration: 3000,
-//     step: function (number1){
-//         this.innerHTML = (number1 + 3).toFixed(0) 
-//     }
-// });
-// $('.number3').animate({ num: 18303 - 3 }, {
-//     duration: 3000,
-//     step: function (number1){
-//         this.innerHTML = (number1 + 3).toFixed(0) 
-//     }
-// });
-// $('.number4').animate({ num: 98 - 3 }, {
-//     duration: 3000,
-//     step: function (number1){
-//         this.innerHTML = (number1 + 3).toFixed(0) 
-//     }
-// });
+$('.number1').animate({ num: 1386 - 3 }, {
+    duration: 3000,
+    step: function (number1){
+        this.innerHTML = (number1 + 3).toFixed(0) 
+    }
+});
+$('.number2').animate({ num: 386 - 3 }, {
+    duration: 3000,
+    step: function (number1){
+        this.innerHTML = (number1 + 3).toFixed(0) 
+    }
+});
+$('.number3').animate({ num: 18303 - 3 }, {
+    duration: 3000,
+    step: function (number1){
+        this.innerHTML = (number1 + 3).toFixed(0) 
+    }
+});
+$('.number4').animate({ num: 98 - 3 }, {
+    duration: 3000,
+    step: function (number1){
+        this.innerHTML = (number1 + 3).toFixed(0) 
+    }
+});
 
 
 
 
 
-// $(".btn").click(function(){
-//     $(".input").toggleClass("active").focus;
-//     $(this).toggleClass("animate");
-//     $(".input").val("");
-//   });
+$(".btn").click(function(){
+    $(".input").toggleClass("active").focus;
+    $(this).toggleClass("animate");
+    $(".input").val("");
+  });
 
 
 
