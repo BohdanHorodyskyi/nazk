72f758649ff78074b2272fa18f8acd5f0728bad8
BohdanHorodyskyi <bohdanhorodyskyi@gmail.com>
2020-07-28 13:46:13 +0300

    second commit

--- script.js
+++ script.js
@@ -22,9 +22,30 @@ $('.number4').animate({ num: 98 - 3 }, {
         this.innerHTML = (number1 + 3).toFixed(0) 
     }
 });
+const menu = document.querySelector('.menu')
+const menuBody = document.querySelector('.menu_body')
+const contain = document.querySelector('.container')
+const rad = document.querySelector('#radio1')
+const rad1 = document.querySelector('.radio11')
+const rad2 = document.querySelector('#radio2')
+const rad22 = document.querySelector('.radio22')
+const rad3 = document.querySelector('#radio3')
+const rad33 = document.querySelector('.radio33')
+const rad4= document.querySelector('#radio4')
+const rad44 = document.querySelector('.radio44')
 
-
-
+rad.onclick = function(){
+    rad1.style.color = '#2596D9'
+}
+rad2.onclick = function(){
+    rad22.style.color = '#2596D9'
+}
+rad3.onclick = function(){
+    rad33.style.color = '#2596D9'
+}
+rad4.onclick = function(){
+    rad44.style.color = '#2596D9'
+}
 
 
 $(".btn").click(function(){
@@ -35,4 +56,9 @@ $(".btn").click(function(){
 
 
 
-
+menu.onclick = function(){
+        contain.classList.toggle('toggle') 
+    
+        menuBody.classList.toggle("toggle1")
+    
+}
